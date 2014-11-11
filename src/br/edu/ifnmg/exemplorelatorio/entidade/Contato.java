843fe9e83a276874e5cd21d97aed0b056abf8a83
Helder Seixas Lima <helderseixas@gmail.com>
2014-11-11 19:21:39 -0200

    Primeiro commit

---
+++ src/br/edu/ifnmg/exemplorelatorio/entidade/Contato.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.exemplorelatorio.entidade;
+
+import java.util.Date;
+
+/**
+ *
+ * @author helder
+ */
+public class Contato {
+    
+    private String nome;
+    private String email;
+    private char sexo;
+    private Date dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }    
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }        
+}
